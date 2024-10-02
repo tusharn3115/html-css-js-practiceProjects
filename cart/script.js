72784818a831e11757aece0f717832e4f3c4d944
Tushar Negi <negitushar81@gmail.com>
2024-10-02 18:06:52 +0530

    shopping app cart functionallity when click on add to cart

--- cart/script.js
+++ cart/script.js
@@ -24,7 +24,7 @@ let products = [
 
 function addProducts() {
   let clutter = "";
-  products.forEach((product) => {
+  products.forEach((product, index) => {
     clutter += `  <div class="product w-fit rounded-xl p-2 bg-white">
                   <div class="image w-[14rem] h-[13rem] bg-zinc-200 rounded-xl">
                   <img class="w-full h-full object-cover"
@@ -37,7 +37,7 @@ function addProducts() {
                               <h3 class="font-semibold w-[50%] opacity-20">${product.heading}</h3>
                               <h4 class="font-semibold mt-2">&#8377; ${product.price}</h4>
                           </div>
-                          <button class="w-10 h-10 rounded-full shader bg-[#4A4563] text-yellow-400"><i class="ri-add-line"></i></button>
+                          <button data-index="${index}" class="add w-10 h-10 rounded-full shader bg-[#4A4563] text-yellow-400"><i data-index="${index}" class="add ri-add-line"></i></button>
                       </div>
                     </div>
                 </div>`;
@@ -45,35 +45,86 @@ function addProducts() {
 
   document.querySelector(".products").innerHTML = clutter;
 }
-addProducts()
+addProducts();
 
-// on click of the product add button  add it to the cart
+// on click of the product add button add it to the cart
+let cart = [];
+function addToCart() {
+  document.querySelector(".products").addEventListener("click", (details) => {
+    if (details.target.classList.contains("add")) {
+      cart.push(products[details.target.dataset.index]);
+      showToast("Product added to cart.");
+    }
+  });
+}
+
+function showCart() {
+  document.querySelector(".carticon").addEventListener("click", () => {
+    document.querySelector(".cartexpnd").style.display = "block";
+
+
+  let clutter = "";
+  cart.forEach((prod) => {
+    clutter += `<div class="flex gap-2 bg-white p-2 rounded-lg">
+           <div class="w-10 h-10 flex-shrink-0 rounded-lg overflow-hidden">
+              <img class="w-full h-full object-cover" src="${prod.image}" />
+           </div>
+           <div>
+              <h3 class="font-semibold">${prod.name}</h3> 
+              <h5 class="text-sm font-semibold opacity-80">&#8377; ${prod.price}</h5>
+           </div>
+        </div>`;
+  });
+
+  document.querySelector(".cartexpnd")
+  .innerHTML = clutter;
+
+});
+
+}
+showCart();
+
+function showToast(message) {
+  const toast = document.getElementById("toast");
+  toast.textContent = message;
+  toast.className = "toast show";
+
+  // Hide the toast after 3 seconds
+  setTimeout(() => {
+    toast.className = toast.className.replace("show", "");
+  }, 3000);
+}
+addToCart();
+
 // show popular or most ordered products
 let popular = [
-    {
-        name: "Combo Chair",
-        heading: "Very Comfortable Chair",
-        price: "18500",
-        image: "https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-        name: "Office Chairs and Table",
-        heading: "Very Comfortable Chair",
-        price: "35000",
-        image: "https://images.pexels.com/photos/416320/pexels-photo-416320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
-    {
-        name: "Fancy Chairs Combo",
-        heading: "Very Comfortable Chair",
-        price: "15700",
-        image: "https://images.pexels.com/photos/827518/pexels-photo-827518.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    },
+  {
+    name: "Combo Chair",
+    heading: "Very Comfortable Chair",
+    price: "18500",
+    image:
+      "https://images.pexels.com/photos/1350789/pexels-photo-1350789.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    name: "Office Chairs and Table",
+    heading: "Very Comfortable Chair",
+    price: "35000",
+    image:
+      "https://images.pexels.com/photos/416320/pexels-photo-416320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    name: "Fancy Chairs Combo",
+    heading: "Very Comfortable Chair",
+    price: "15700",
+    image:
+      "https://images.pexels.com/photos/827518/pexels-photo-827518.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
 ];
 
 function addPopularProducts() {
-    let clutter = "";
-    popular.forEach((product) => {
-      clutter += `<div class="popular bg-white p-2 rounded-2xl flex items-start gap-3 w-[60%] flex-shrink-0">
+  let clutter = "";
+  popular.forEach((product) => {
+    clutter += `<div class="popular bg-white p-2 rounded-2xl flex items-start gap-3 w-[60%] flex-shrink-0">
                     <div class="w-20 h-20 bg-red-500 flex-shrink-0 rounded-2xl border-4 border-white overflow-hidden">
                         <img class="w-full h-full object-cover"
                             src="${product.image}"
@@ -85,8 +136,8 @@ function addPopularProducts() {
                         <h4 class="mt-3 font-semibold text-zinc-500">&#8377; ${product.price}</h4>
                     </div>
                 </div>`;
-    });
-  
-    document.querySelector(".populars").innerHTML = clutter;
-  }
-addPopularProducts()
+  });
+
+  document.querySelector(".populars").innerHTML = clutter;
+}
+addPopularProducts();
